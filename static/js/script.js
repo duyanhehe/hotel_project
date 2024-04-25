2592d062789f4f78663215b9596237ec301947b3
ManhQuang <nguyenquangmanh.6264@gmail.com>
2024-04-25 12:07:24 +0700

    donut chart dashboard

--- static/js/script.js
+++ static/js/script.js
@@ -89,3 +89,47 @@ async function validateForm() {
 
     return true;  // Form validation successful
 }
+
+
+//chart js
+const chartData = {
+    labels: ["Python", "Java", "Javascript", "C#", "Others"],
+    data: [30, 17, 10, 7, 36]
+};
+
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programming-stats .details ul ");
+
+new Chart(myChart, {
+    type:"doughnut",
+    data: {
+        labels: chartData.labels,
+        datasets: [
+            {
+                label: "Language Popularity",
+                data: chartData.data,
+            },
+        ],
+    },
+    options: {
+        hoverBorderWidth: 10,
+        plugins: {
+            legend: {
+                display: true
+            },
+        },
+    },
+}); 
+
+const populateUL = () => {
+    chartData.labels.forEach((l, i) => {
+        let li = document.createElement("li");
+        li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+        ul.appendChild(li);
+
+    
+
+    });
+};
+
+populateUL();
